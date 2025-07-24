26f9646fd88e4f4f1198364ed9bbcbb690a7a27d
PSolntsev <PSolntsev@synergy.ru>
2025-07-24 14:29:38 +0300

    fix

--- .distr/templates/default.php
+++ .distr/templates/default.php
@@ -30,6 +30,7 @@ $jsVersion = filemtime($_SERVER['DOCUMENT_ROOT'] . '/js/script.js');
     <meta property="og:image" content="//<?= $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ?>img/common/share.jpg?v=1.0.1">
     <link rel="image_src" href="//<?= $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ?>img/common/share.jpg?v=1.0.1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="/favicon.ico" type="image/x-icon" rel="icon">
     <link rel="stylesheet" href="css/style.css?v=<?= $cssVersion ?>">
 </head>
