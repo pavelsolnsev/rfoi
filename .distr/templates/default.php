4e4f0e11bffeca97412733dac8e3a1b13254bd81
PSolntsev <PSolntsev@synergy.ru>
2026-01-17 14:29:37 +0300

    заменил фавикон

--- .distr/templates/default.php
+++ .distr/templates/default.php
@@ -32,7 +32,7 @@ $jsVersion = filemtime($_SERVER['DOCUMENT_ROOT'] . '/js/script.js');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
-    <link href="/RFOI-ng.ico" type="image/x-icon" rel="icon">
+    <link href="/favicon.ico" type="image/x-icon" rel="icon">
     <link rel="stylesheet" href="css/style.css?v=<?= $cssVersion ?>">
 </head>
 
